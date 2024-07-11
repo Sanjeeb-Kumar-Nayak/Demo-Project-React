2572a53ccd63ebe1ccfe585076961b2edf991886
Sanjeeb Kumar Nayak <97420921+Sanjeeb-Kumar-Nayak@users.noreply.github.com>
2024-07-11 15:51:00 +0530

    change

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,35 +2,46 @@ import React from "react";
 import "./index.css";
 import { Link, useNavigate } from "react-router-dom";
 import { Row, Col, Button } from "react-bootstrap";
-import sknLogo from "../../assets/images/sknLogo.jpg";
+import sknLogo from "../../assets/images/tree.png";
 
 export const Header = () => {
   const loginButtonStyle = {
     width: "6rem",
-    backgroundColor: "wheat",
-    color: "Black",
-    borderColor: "wheat",
+    backgroundColor: "white",
+    color: "grey",
+    borderColor: "rgb(220, 241, 241)",
+    fontWeight: "800"
   };
 
   const signupButtonStyle = {
     width: "6rem",
-    backgroundColor: "#4aaf51",
-    color: "white",
-    borderColor: "#4aaf51",
+    backgroundColor: "white",
+    color: "blueviolet",
+    borderColor: "rgb(220, 241, 241)",
+    fontWeight: "800"
   };
 
   return (
     <Row className="header">
-      <Col md={2}>
-        <img src={sknLogo} height={"35px"} width={"50px"} />
+      <Col md={2} style={{ textAlign: "center" }}>
+        <img src={sknLogo} height={"70px"} width={"70px"} />
       </Col>
       <Col
         md={7}
-        style={{ textAlign: "center", fontSize: "25px", color: "wheat" }}
+        style={{
+          textAlign: "center",
+          paddingTop: "15px",
+          fontSize: "25px",
+          color: "green",
+          fontWeight: "1000"
+        }}
       >
-        SKN PRIVATE LIMITED
+        Nayak Engineering Private Limited
       </Col>
-      <Col md={3} style={{ textAlign: "right" }}>
+      <Col
+        md={3}
+        style={{ textAlign: "right", paddingTop: "15px", paddingRight: "30px" }}
+      >
         <Button style={loginButtonStyle}>Log In</Button>
         <Button className="ms-3" style={signupButtonStyle}>
           Sign Up
